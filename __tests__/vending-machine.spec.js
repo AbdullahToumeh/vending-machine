a257fa70c2eb92d3244cf7099200047375c9629c
Dakota <dakota.paige@hotmail.com>
2018-05-25 10:52:50 -0700

    Wrote tests for refilling items

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -11,13 +11,36 @@ describe('Vending Machine', () => {
       '1.00': 4,
       '2.00': 5
     };
-    test.itemData = JSON.stringify(ItemData);
+    test.refillKey = 'A1';
+    test.refillAmount = 5;
+    test.refillData = {
+      key: test.refillKey,
+      'item-name': 'All dressed chips',
+      price: 1.25,
+      stock: 10
+    };
+    test.itemData = JSON.parse(JSON.stringify(ItemData));
     test.subject = new VendingMachine(test.itemData, test.machineChange);
   });
+
   describe('When a user requests to see inventory', () => {
     it('Should display a list of the inventory with all item information', () => {
       const result = test.subject.printInventory();
       expect(result).toEqual(test.itemData);
+    });
+  });
+
+  describe('When maintenance refills an item using the item key', () => {
+    it('Should return an error if no item exists at that location', () => {
+      const result = test.subject.refillInventory('C1', 5);
+      expect(result).toEqual('No item exists at that key');
+    });
+    it('Should return feedback on the new inventory of that item if it exists.', () => {
+      const result = test.subject.refillInventory(
+        test.refillKey,
+        test.refillAmount
+      );
+      expect(result).toEqual(test.refillData);
     });
   });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -8,7 +8,20 @@ class VendingMachine {
     return this.itemData;
   }
 
-  refillInventory() {}
+  refillInventory(itemKey, itemAmount) {
+    const refillItem = this.itemData.filter(item => item.key === itemKey);
+
+    if (refillItem.length) {
+      for (let i = 0; i <= this.itemData.length; i++) {
+        if (this.itemData[i].key === refillItem[0].key) {
+          this.itemData[i].stock += itemAmount;
+          return this.itemData[i];
+        }
+      }
+    }
+
+    return 'No item exists at that key';
+  }
 
   reSupplyChange() {}
 
