c324ce659a5fd39df16c8ba5ac7c03c7d20fdc19
Dakota <dakota.paige@hotmail.com>
2018-05-26 13:36:23 -0700

    Started on a change algorithm, its mostly working just need to edit the output

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -2,23 +2,56 @@ const VendingMachine = require('../src/vending-machine.js');
 const ItemData = require('../__mocks__/item-data.json');
 
 describe('Vending Machine', () => {
+  let test;
   beforeEach(() => {
     test = {};
     test.machineChange = {
-      '0.05': 5,
-      '0.10': 5,
-      '0.25': 8,
+      '2.00': 5,
       '1.00': 4,
-      '2.00': 5
+      '0.25': 8,
+      '0.10': 5,
+      '0.05': 5
     };
+
     test.refillKey = 'A1';
     test.refillAmount = 5;
     test.refillData = {
       key: test.refillKey,
-      'item-name': 'All dressed chips',
+      itemname: 'All dressed chips',
       price: 1.25,
       stock: 10
     };
+
+    test.refillChange = {
+      '0.05': 5,
+      '0.10': 10,
+      '0.25': 5,
+      '1.00': 6,
+      '2.00': 5
+    };
+    test.refillOutputData = {
+      '0.05': 10,
+      '0.10': 15,
+      '0.25': 13,
+      '1.00': 10,
+      '2.00': 10
+    };
+
+    test.purchaseItemButton = 'A2';
+    test.purchaseItemChange = {
+      '0.25': 2,
+      '1.00': 1
+    };
+    test.purchaseItemExtraChange = {
+      '2.00': 1
+    };
+
+    test.purchaseItemReturn = { item: 'Sour Patch Kids', change: {} };
+    test.purchaseItemExtraChangeReturn = {
+      item: 'Sour Patch Kids',
+      change: { '0.25': 2 }
+    };
+
     test.itemData = JSON.parse(JSON.stringify(ItemData));
     test.subject = new VendingMachine(test.itemData, test.machineChange);
   });
@@ -42,5 +75,51 @@ describe('Vending Machine', () => {
       );
       expect(result).toEqual(test.refillData);
     });
+  });
+
+  describe('When maintenance refills change in the machine', () => {
+    it('Should return feedback on the new change amount in the machine', () => {
+      const result = test.subject.reSupplyChange(test.refillChange);
+      expect(result).toEqual(test.refillOutputData);
+    });
+    it('Should return an error if they enter an invalid change amount', () => {
+      const result = test.subject.reSupplyChange(10);
+      expect(result).toEqual('That is an invalid change amount');
+    });
+  });
+
+  describe('When a user enters exact change for their button selection', () => {
+    it('Should return the item with no change', () => {
+      const result = test.subject.dispenseInventory(
+        test.purchaseItemButton,
+        test.purchaseItemChange
+      );
+      expect(result).toEqual(test.purchaseItemReturn);
+    });
+  });
+  describe('When a user enters insufficient funds for their button selection', () => {
+    it('Should return an error and the entirety of their change back', () => {});
+  });
+  describe('When a user enters in more change than necessary for their button selection', () => {
+    it('Should return the item', () => {
+      const { item } = test.subject.dispenseInventory(
+        test.purchaseItemButton,
+        test.purchaseItemExtraChange
+      );
+      expect(item).toEqual(test.purchaseItemExtraChangeReturn.item);
+    });
+    it('Should return the change difference between what they entered and what they purchased', () => {
+      const { change } = test.subject.dispenseInventory(
+        test.purchaseItemButton,
+        test.purchaseItemExtraChange
+      );
+      expect(change).toEqual(test.purchaseItemExtraChangeReturn.change);
+    });
+  });
+  describe('When a user clicks an invalid button', () => {
+    it('Should return an error and the entirety of their change back', () => {});
+  });
+  describe('When a user chooses an item that has no inventory', () => {
+    it('Should return an error and the entirety of their change back', () => {});
   });
 });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -23,9 +23,62 @@ class VendingMachine {
     return 'No item exists at that key';
   }
 
-  reSupplyChange() {}
+  reSupplyChange(refillChange) {
+    if (typeof refillChange !== 'object') {
+      return 'That is an invalid change amount';
+    }
 
-  dispenseInventory() {}
+    const changeKeys = Object.keys(refillChange);
+
+    changeKeys.map(
+      changeKey => (this.currentChange[changeKey] += refillChange[changeKey])
+    );
+
+    return this.currentChange;
+  }
+
+  dispenseInventory(clickedButton, inputChange) {
+    let returnedItem = { item: '', change: {} };
+    let totalGiven = 0;
+
+    let totalChange = 0;
+
+    if (typeof inputChange !== 'object') {
+      return 0;
+    }
+
+    //create the total given change based on the change they inputted
+    Object.keys(inputChange).map(
+      coin => (totalGiven += Number(coin) * inputChange[coin])
+    );
+
+    for (let i = 0; i < this.itemData.length; i++) {
+      if (this.itemData[i].key === clickedButton) {
+        returnedItem.item = this.itemData[i].itemname;
+        if (this.itemData[i].price === totalGiven) {
+          return returnedItem;
+        } else if (this.itemData[i].price > totalGiven) {
+          returnedItem.item = 'Insufficient Funds';
+          returnedItem.change = inputChange;
+          return returnedItem;
+        } else {
+          totalChange = totalGiven - this.itemData[i].price;
+        }
+      }
+    }
+
+    //loops over current change in machine (which is in order from greatest to least - therefore should return smallest amount of coins) and adds it to returned change if theres still change left for that value and its less than or equal to the change they need back.
+    for (let i in this.currentChange) {
+      returnedItem.change[i] = 0;
+      while (this.currentChange[i] > 0 && Number(i) <= totalChange) {
+        totalChange -= Number(i);
+        returnedItem.change[i] += 1;
+        console.log(returnedItem);
+      }
+    }
+
+    return returnedItem;
+  }
 
   returnChange() {}
 }
